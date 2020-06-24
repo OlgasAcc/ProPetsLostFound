6d5cd67c64bf2e0ad8df1241f271ee4853125dd1
olgak_tdvqm06 <olgak_tdvqm06@192.168.1.6>
2020-06-24 22:55:42 +0300

    24_06_2020
    cleaned pom

--- ProPetsLostFoundService/src/main/java/proPets/lostFound/dto/PostToConvertDto.java
+++ ProPetsLostFoundService/src/main/java/proPets/lostFound/dto/PostToConvertDto.java
@@ -1,9 +1,5 @@
 package proPets.lostFound.dto;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,11 +13,10 @@ import lombok.Setter;
 public class PostToConvertDto { 
 	
 	String id; //post id
+	String email; //authorId
 	String flag;
 	String type;
 	String address;
 	String distinctiveFeatures;
 	String[] picturesURLs;
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	LocalDateTime dateOfPublish;
 }
